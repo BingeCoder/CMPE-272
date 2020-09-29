48405a29186e6d63de95d4f041dc99c24f80af17
GunjanSrivastava <70232901+GunjanSrivastava@users.noreply.github.com>
2020-09-29 12:09:05 -0700

    Added files for create post api integration

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'q3jlBIfKjejp2EXaD9l86Okin',
+    consumer_secret:      'SKi9UnV6yFbzRBl09cFWloeqPaMZKoJOG51Y9ypWblDSvmYEQ3',
+    access_token:         '1306397020106547201-CEdWK9PVEYv9gpctatyJcsqi5HbELY',
+    access_token_secret:  'a8NBnVNgcXk6n2PA0l4G50dsw9QYJ9uJRfQ8goek9DNr3'
+}
